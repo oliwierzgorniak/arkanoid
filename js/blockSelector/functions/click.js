c4c80cd931307f7c6de895bc113776bc11272dcc
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2022-06-15 23:08:17 +0200

    deselecting after adding block

--- js/blockSelector/functions/click.js
+++ js/blockSelector/functions/click.js
@@ -6,9 +6,14 @@ function click(e) {
   // https://www.tutorialspoint.com/How-to-copy-the-content-of-one-HTML5-Canvas-to-another-Canvas-locally
 
   BoardData.selectedBlocks.forEach((blockElement) => {
+    // dehighlighting
+    blockElement.style.border = BoardData.blockBorders[0];
+
     let ctx = blockElement.getContext("2d");
     ctx.drawImage(block, 0, 0);
   });
+
+  BoardData.selectedBlocks = [];
 }
 
 function addCanvasToBlock() {}
